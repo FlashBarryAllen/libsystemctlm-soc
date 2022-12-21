5dfc1322ca319b8b88953969674eb727dd6a8db6
FlashBarryAllen <931415151@qq.com>
2022-12-21 10:30:22 +0000

    format change

--- tests/example-rtl-axi4/example-rtl-axi4.cc
+++ tests/example-rtl-axi4/example-rtl-axi4.cc
@@ -52,134 +52,135 @@ using namespace utils;
 #define CONNECT_DUT(DUT, SIGS, SIGNAME) DUT.s00_axi_ ## SIGNAME(SIGS.SIGNAME)
 
 TrafficDesc transactions(merge({
-	// Write something to address 8
-        Write(8, DATA(0x1, 0x2, 0x3, 0x4)),
-	// Read it back and check that we get the expected data.
-        Read(8, 4),
-		Expect(DATA(0x1, 0x2, 0x3, 0x4), 4)
+    // Write something to address 8
+    Write(8, DATA(0x1, 0x2, 0x3, 0x4)),
+    // Read it back and check that we get the expected data.
+    Read(8, 4),
+    Expect(DATA(0x1, 0x2, 0x3, 0x4), 4)
 }));
 
 AXIPCConfig checker_config()
 {
-	AXIPCConfig cfg;
+    AXIPCConfig cfg;
 
-	cfg.enable_all_checks();
+    cfg.enable_all_checks();
 
-	return cfg;
+    return cfg;
 }
 
 // Top simulation module.
 SC_MODULE(Top)
 {
-	sc_clock clk;
-	sc_signal<bool> rst_n; // Active low.
+    sc_clock clk;
+    sc_signal<bool> rst_n; // Active low.
 
-	TLMTrafficGenerator tg;
-	AXISignals<10, 128 > signals;
-	tlm2axi_bridge<10, 128 > bridge;
-        AXIProtocolChecker<10, 128 > checker;
-	// dut is the RTL AXI device we're testing.
-	Vaxifull_dev dut;
+    TLMTrafficGenerator tg;
+    AXISignals<10, 128 > signals;
+    tlm2axi_bridge<10, 128 > bridge;
+    AXIProtocolChecker<10, 128 > checker;
+    // dut is the RTL AXI device we're testing.
+    Vaxifull_dev dut;
 
-	Top(sc_module_name name) :
-		clk("clk", sc_time(1, SC_US)),
-		rst_n("rst_n"),
-		tg("traffic_generator"),
-		signals("signals"),
-		bridge("bridge"),
-                checker("checker", checker_config()),
-		dut("dut")
-	{
-		// Configure the Traffic generator.
-		tg.setStartDelay(sc_time(10, SC_US));
-		tg.enableDebug();
-		tg.addTransfers(transactions);
-		tg.socket.bind(bridge.tgt_socket);
+    Top(sc_module_name name) :
+        clk("clk", sc_time(1, SC_US)),
+        rst_n("rst_n"),
+        tg("traffic_generator"),
+        signals("signals"),
+        bridge("bridge"),
+        checker("checker", checker_config()),
+        dut("dut")
+    {
+        // Configure the Traffic generator.
+        tg.setStartDelay(sc_time(10, SC_US));
+        tg.enableDebug();
+        tg.addTransfers(transactions);
+        tg.socket.bind(bridge.tgt_socket);
 
-		// Wire up the clock and reset signals.
-		bridge.clk(clk);
-		bridge.resetn(rst_n);
-		checker.clk(clk);
-		checker.resetn(rst_n);
-		dut.s00_axi_aclk(clk);
-		dut.s00_axi_aresetn(rst_n);
+        // Wire up the clock and reset signals.
+        bridge.clk(clk);
+        bridge.resetn(rst_n);
+        checker.clk(clk);
+        checker.resetn(rst_n);
+        dut.s00_axi_aclk(clk);
+        dut.s00_axi_aresetn(rst_n);
 
-		// Wire-up the bridge and checker.
-		signals.connect(bridge);
-		signals.connect(checker);
+        // Wire-up the bridge and checker.
+        signals.connect(bridge);
+        signals.connect(checker);
 
-		// Since the AXI Dut doesn't use the same naming conventions
-		// as AXISignals, we need to manually connect everything.
-		dut.s00_axi_awvalid(signals.awvalid);
-		dut.s00_axi_awready(signals.awready);
-		dut.s00_axi_awaddr(signals.awaddr);
-		dut.s00_axi_awprot(signals.awprot);
-		dut.s00_axi_awuser(signals.awuser);
-		dut.s00_axi_awregion(signals.awregion);
-		dut.s00_axi_awqos(signals.awqos);
-		dut.s00_axi_awcache(signals.awcache);
-		dut.s00_axi_awburst(signals.awburst);
-		dut.s00_axi_awsize(signals.awsize);
-		dut.s00_axi_awlen(signals.awlen);
-		dut.s00_axi_awid(signals.awid);
-		dut.s00_axi_awlock(signals.awlock);
+        // Since the AXI Dut doesn't use the same naming conventions
+        // as AXISignals, we need to manually connect everything.
+        dut.s00_axi_awvalid(signals.awvalid);
+        dut.s00_axi_awready(signals.awready);
+        dut.s00_axi_awaddr(signals.awaddr);
+        dut.s00_axi_awprot(signals.awprot);
+        dut.s00_axi_awuser(signals.awuser);
+        dut.s00_axi_awregion(signals.awregion);
+        dut.s00_axi_awqos(signals.awqos);
+        dut.s00_axi_awcache(signals.awcache);
+        dut.s00_axi_awburst(signals.awburst);
+        dut.s00_axi_awsize(signals.awsize);
+        dut.s00_axi_awlen(signals.awlen);
+        dut.s00_axi_awid(signals.awid);
+        dut.s00_axi_awlock(signals.awlock);
 
-		dut.s00_axi_arvalid(signals.arvalid);
-		dut.s00_axi_arready(signals.arready);
-		dut.s00_axi_araddr(signals.araddr);
-		dut.s00_axi_arprot(signals.arprot);
-		dut.s00_axi_aruser(signals.aruser);
-		dut.s00_axi_arregion(signals.arregion);
-		dut.s00_axi_arqos(signals.arqos);
-		dut.s00_axi_arcache(signals.arcache);
-		dut.s00_axi_arburst(signals.arburst);
-		dut.s00_axi_arsize(signals.arsize);
-		dut.s00_axi_arlen(signals.arlen);
-		dut.s00_axi_arid(signals.arid);
-		dut.s00_axi_arlock(signals.arlock);
+        dut.s00_axi_arvalid(signals.arvalid);
+        dut.s00_axi_arready(signals.arready);
+        dut.s00_axi_araddr(signals.araddr);
+        dut.s00_axi_arprot(signals.arprot);
+        dut.s00_axi_aruser(signals.aruser);
+        dut.s00_axi_arregion(signals.arregion);
+        dut.s00_axi_arqos(signals.arqos);
+        dut.s00_axi_arcache(signals.arcache);
+        dut.s00_axi_arburst(signals.arburst);
+        dut.s00_axi_arsize(signals.arsize);
+        dut.s00_axi_arlen(signals.arlen);
+        dut.s00_axi_arid(signals.arid);
+        dut.s00_axi_arlock(signals.arlock);
 
-		dut.s00_axi_wvalid(signals.wvalid);
-		dut.s00_axi_wready(signals.wready);
-		dut.s00_axi_wdata(signals.wdata);
-		dut.s00_axi_wstrb(signals.wstrb);
-		dut.s00_axi_wuser(signals.wuser);
-		dut.s00_axi_wlast(signals.wlast);
+        dut.s00_axi_wvalid(signals.wvalid);
+        dut.s00_axi_wready(signals.wready);
+        dut.s00_axi_wdata(signals.wdata);
+        dut.s00_axi_wstrb(signals.wstrb);
+        dut.s00_axi_wuser(signals.wuser);
+        dut.s00_axi_wlast(signals.wlast);
 
-		dut.s00_axi_bvalid(signals.bvalid);
-		dut.s00_axi_bready(signals.bready);
-		dut.s00_axi_bresp(signals.bresp);
-		dut.s00_axi_buser(signals.buser);
-		dut.s00_axi_bid(signals.bid);
+        dut.s00_axi_bvalid(signals.bvalid);
+        dut.s00_axi_bready(signals.bready);
+        dut.s00_axi_bresp(signals.bresp);
+        dut.s00_axi_buser(signals.buser);
+        dut.s00_axi_bid(signals.bid);
 
-		dut.s00_axi_rvalid(signals.rvalid);
-		dut.s00_axi_rready(signals.rready);
-		dut.s00_axi_rdata(signals.rdata);
-		dut.s00_axi_rresp(signals.rresp);
-		dut.s00_axi_ruser(signals.ruser);
-		dut.s00_axi_rid(signals.rid);
-		dut.s00_axi_rlast(signals.rlast);
-	}
+        dut.s00_axi_rvalid(signals.rvalid);
+        dut.s00_axi_rready(signals.rready);
+        dut.s00_axi_rdata(signals.rdata);
+        dut.s00_axi_rresp(signals.rresp);
+        dut.s00_axi_ruser(signals.ruser);
+        dut.s00_axi_rid(signals.rid);
+        dut.s00_axi_rlast(signals.rlast);
+    }
 };
 
 int sc_main(int argc, char *argv[])
 {
-	Verilated::commandArgs(argc, argv);
-	Top top("Top");
+    Verilated::commandArgs(argc, argv);
+    Top top("Top");
 
-	sc_trace_file *trace_fp = sc_create_vcd_trace_file(argv[0]);
+    sc_trace_file *trace_fp = sc_create_vcd_trace_file(argv[0]);
 
-	top.signals.Trace(trace_fp);
+    top.signals.Trace(trace_fp);
 
-	// Reset is active low. Emit a reset cycle.
-	top.rst_n.write(false);
-	sc_start(4, SC_US);
-	top.rst_n.write(true);
+    // Reset is active low. Emit a reset cycle.
+    top.rst_n.write(false);
+    sc_start(4, SC_US);
+    top.rst_n.write(true);
 
-	sc_start(14, SC_US);
-	sc_stop();
+    sc_start(14, SC_US);
+    sc_stop();
 
-	if (trace_fp) {
-		sc_close_vcd_trace_file(trace_fp);
-	}
-	return 0;
+    if (trace_fp) {
+        sc_close_vcd_trace_file(trace_fp);
+    }
+    
+    return 0;
 }
